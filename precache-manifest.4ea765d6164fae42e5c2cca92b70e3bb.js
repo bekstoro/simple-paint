6d28bc4a6cb2b0d454acaee7e88b28e3a4dfe0a4
bekstoro <bekstoro@gmail.com>
2020-03-16 19:50:21 +0300

    Updates

---
+++ precache-manifest.4ea765d6164fae42e5c2cca92b70e3bb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4bf94d97aecb804f9ee6e49c012d9f4",
+    "url": "/simple-paint/index.html"
+  },
+  {
+    "revision": "a2b7ddb4bc03b104f32c",
+    "url": "/simple-paint/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "6689689fccc43536612f",
+    "url": "/simple-paint/static/js/2.91a320bf.chunk.js"
+  },
+  {
+    "revision": "c099810c3c102db7a44755001529c804",
+    "url": "/simple-paint/static/js/2.91a320bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a2b7ddb4bc03b104f32c",
+    "url": "/simple-paint/static/js/main.5dff9b26.chunk.js"
+  },
+  {
+    "revision": "16475b9585b9a6fbc468",
+    "url": "/simple-paint/static/js/runtime-main.51a856a3.js"
+  }
+]);
